d1be62ceeab2a1bf8fc03f7f6ba8f3062365c50c
Mauro Adauto <mauroadauto9@gmail.com>
2023-09-23 22:18:54 -0500

    Programa aplicado al ultimo video

---
+++ biblioteca-deteccion/prueba2.py
@@ -0,0 +1,66 @@
+import cv2
+import pickle
+import numpy as np
+
+# Definir los rangos de color para los asientos rojos y grises en formato BGR
+red_lower = np.array([0, 0, 150])
+red_upper = np.array([100, 100, 255])
+gray_lower = np.array([150, 150, 150])
+gray_upper = np.array([220, 220, 220])
+
+asientos = []
+with open('biblioteca-pkl/biblioteca.pkl', 'rb') as file:
+    asientos = pickle.load(file)
+
+video = cv2.VideoCapture('biblioteca-deteccion/biblioteca.mp4')
+
+while True:
+    check, img = video.read()
+
+    for x, y, w, h in asientos:
+        espacio = img[y:y+h, x:x+w]
+        
+        # Convertir el espacio a formato HSV
+        espacio_hsv = cv2.cvtColor(espacio, cv2.COLOR_BGR2HSV)
+
+        # Calcular las máscaras para los colores rojo y gris
+        mask_red = cv2.inRange(espacio, red_lower, red_upper)
+        mask_gray = cv2.inRange(espacio, gray_lower, gray_upper)
+
+        # Contar los píxeles en cada máscara
+        count_red = cv2.countNonZero(mask_red)
+        count_gray = cv2.countNonZero(mask_gray)
+        
+        other_color = w * h - (count_gray + count_red)
+        
+        gen_count = w * h
+
+        cv2.putText(img, ('Red: ' + str(count_red)), (x, y + h - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        cv2.putText(img, ('Gray: ' + str(count_gray)), (x, y + h - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.putText(img, ('General: ' + str(gen_count)), (x, y + h - 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.putText(img, ('Other color: ' + str(other_color)), (x, y + h - 45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        
+        """if count_gray < count_red:
+            cv2.putText(img, 'OCUPADO', (x, y + h - w), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        else:
+            cv2.putText(img, 'VACIO', (x, y + h - w), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)"""
+            
+        if count_red > 300:
+            if count_red + count_gray < other_color / 2:
+                cv2.putText(img, 'OCUPADO', (x, y + h - w), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            else:
+                cv2.putText(img, 'VACIO', (x, y + h - w), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('video', img)
+    
+    key = cv2.waitKey(10)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
+video.release()
+ 
---
+++ biblioteca-pkl/main_1.py
@@ -0,0 +1,17 @@
+import cv2
+import pickle
+
+img = cv2.imread('biblioteca-pkl/ss-biblioteca.png')
+
+espacios = []
+
+for x in range(4):
+    espacio = cv2.selectROI('espacio', img, False)
+    cv2.destroyWindow('espacio')
+    espacios.append(espacio)
+
+for x, y, w, h in espacios:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+
+with open('biblioteca-pkl/biblioteca.pkl','wb') as file:
+    pickle.dump(espacios, file)
